513a5a05a9d133259b7b48e97bc8895005d98ba6
Chris MacNaughton <chmacnaughton@gmail.com>
2016-03-07 08:26:03 -0500

    lint fixes and gitlab

--- reactive/vault.py
+++ reactive/vault.py
@@ -1,26 +1,21 @@
 import shutil
 import os
 
-from charmhelpers.core import hookenv, unitdata
-from charmhelpers.core.hookenv import (
-    leader_set, leader_get,
-    is_leader,
-)
-from charmhelpers.core.host import log, mkdir
+from charmhelpers.core import hookenv
+from charmhelpers.core.hookenv import leader_get
+from charmhelpers.core.host import mkdir
 from charmhelpers.core.templating import render
 from charms.reactive import when, when_not, hook
-from charms.reactive import is_state, set_state, remove_state
+from charms.reactive import set_state, remove_state
 
 from charmhelpers.core.host import (
-    adduser,
     service_stop,
     service_restart,
-    service_running,
-    service_start,
 )
 
 import charms.leadership
 import hvac
+
 
 @when('consul.connected')
 @when_not('vault.ready')
@@ -41,17 +36,21 @@ def setup_vault(consul):
 def vault_ready():
     # client = hvac.Client()
     client = hvac.Client(url='http://localhost:8200')
+    try:
+        if not client.is_initialized():
+            shares = 1
+            threshold = 1
 
-    if not client.is_initialized():
-        shares = 1
-        threshold = 1
+            result = client.initialize(shares, threshold)
+            client.token = result['root_token']
+            client.unseal(result['keys'][0])
+            charms.leadership.leader_set(
+                root_token=result['root_token'],
+                key=result['keys'][0])
+    except:
+        hookenv.log(
+            "Had a problem with Vault initialization, will try again soon")
 
-        result = client.initialize(shares, threshold)
-        client.token = result['root_token']
-        client.unseal(result['keys'][0])
-        charms.leadership.leader_set(
-            root_token=result['root_token'],
-            key=result['keys'][0])
 
 @when('vault.token.requested', 'leadership.set.root_token')
 def generate_tokens(vault):
@@ -60,9 +59,12 @@ def generate_tokens(vault):
     client.token = leader_get('root_token')
     for service, token in vault.requested_tokens():
         if token:
-            client.create_token(id=token, policies=['root'], display_name=service)
+            client.create_token(
+                id=token, policies=['root'], display_name=service)
         else:
-            token = client.create_token(policies=['root'], display_name=service)['auth']['client_token']
+            token = client.create_token(
+                policies=['root'], display_name=service
+                )['auth']['client_token']
 
         vault.provide_token(
             service=service,
@@ -100,7 +102,9 @@ def stop():
 @hook('install')
 def install():
     hookenv.log('Installing vault')
-    shutil.copyfile('{}/files/vault-0.5.0'.format(hookenv.charm_dir()), '/tmp/vault')
+    shutil.copyfile(
+        '{}/files/vault-0.5.0'.format(hookenv.charm_dir()),
+        '/tmp/vault')
     mkdir('/usr/local/bin')
     shutil.move('/tmp/vault', '/usr/local/bin/vault')
     os.chmod('/usr/local/bin/vault', 0o755)
